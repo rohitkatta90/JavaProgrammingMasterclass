c2c7a9a179668e805aafd060bbe3d944708b2659
Rohit Katta <rkkatta@amazon.com>
2021-04-28 00:24:57 +0530

    committing folder

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+
+        long miles = SpeedConverter.toMilesPerHour(10.5);
+        System.out.println("Miles = "+miles);
+
+        SpeedConverter.printConversion(10.5);
+    }
 }
